d055f35e1e0bcff5b7d677e7f42faf65a8f4ed94
Nikolay Starikov <nickolay-starikov@ya.ru>
2019-08-22 21:11:16 +0300

    basic markup and fonts

---
+++ components/AnimatedImage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    img
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AnimatedImage'
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container">
+    <div class="img">
+      <AnimatedImage />
+    </div>
+    <div class="description">
+      <div class="date">
+        {{ date }}
+      </div>
+      <h1 class="title">
+        <span class="title__descr">
+          {{ title.text }}
+        </span>
+        <span class="title__name">
+          {{ title.name }}
+        </span>
+      </h1>
+      <div class="main-img">
+        <img src="" alt="">
+      </div>
+      <h2 class="subtitle">
+        {{ subtitle }}
+      </h2>
+      <ul class="experts">
+        <li
+          v-for="(expert, index) in experts"
+          :key="index"
+          class="experts__item"
+        >
+          {{ expert }}
+        </li>
+      </ul>
+      <a
+        href="http://roly-poly.com.ua"
+        class="link"
+      >
+        {{ btnText }}
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+import AnimatedImage from '~/components/AnimatedImage'
+
+export default {
+  components: {
+    AnimatedImage
+  },
+  async asyncData ({ $axios }) {
+    const data = await $axios.$get('/data.json')
+
+    return {
+      ...data
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import '../assets/variables.scss';
+
+</style>
